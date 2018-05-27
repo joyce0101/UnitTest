f60020ddbaeedbf0cc300f274177c497fb4c8068
joyce0101 <13759969768@163.com>
2018-05-27 22:54:47 +0800

    modify error

--- test_suit.py
+++ test_suit.py
@@ -1,7 +1,7 @@
 import unittest
 from test_func import *
 if __name__=='__main__':
-    suite=unittest.TestCase()
+    suite=unittest.TestSuite()
     tests=[Test_Func("test_add"),Test_Func("test_postmessage")]
     suite.addTests(tests)
     runner=unittest.TextTestRunner()
