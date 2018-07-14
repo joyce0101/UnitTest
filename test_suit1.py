ab5b20db438e32c9ad4f815ad3d1153e891c098d
joyce0101 <13759969768@163.com>
2018-07-14 23:06:33 +0800

    update

--- test_suit1.py
+++ test_suit1.py
@@ -4,6 +4,7 @@ if __name__=='__main__':
     suite=unittest.TestSuite()
     tests=[Test_Func("test_add"),Test_Func("test_postmessage")]
     suite.addTests(tests)
+	#with的用法，需要注意和学习一下
     with open('UnittestReport.txt','a') as f:
         runner=unittest.TextTestRunner(stream=f,verbosity=2)
         runner.run(suite)
